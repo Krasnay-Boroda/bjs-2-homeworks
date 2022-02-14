39c274dd7d084ffdea46899eade2c4a4ed8ed811
Arkadiy Podorozhnyy <Red.Bard@yandex.ru>
2022-02-14 20:00:51 +0300

    Changed tast

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,31 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+    let arr = [];
+    let rootFirst;
+    let rootSecond;
+    let d = b ** 2 - 4 * a * c;
+    
+    if (d > 0) {
+        rootFirst = (-b + Math.sqrt(d)) / (2 * a);
+        rootSecond = (-b - Math.sqrt(d)) / (2 * a);
+        arr.push (rootFirst);
+        arr.push (rootSecond);
+        return arr;
+
+    } else if (d < 0) { 
+      return arr = [];
+    } else {
+        rootFirst = -b/(2*a);
+        arr.push (rootFirst);
+        return arr;
+    }
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+
+
+
+
+
+
